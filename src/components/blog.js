ca26b1fc5ba6b643da8b62ab6639e1b669923a78
Emma Rafkin <emmarafkin@gmail.com>
2020-06-20 17:07:00 -0500

    keywords there but ugly

--- src/components/blog.js
+++ src/components/blog.js
@@ -49,40 +49,70 @@ class Blog extends React.Component {
           : <div />
               }
         <div className="postContainer">
-          {this.state.keyword === 'asdf' ? <div /> : <p>Keyword selected: {this.state.keyword}</p> }
-          {this.props.blogs.map((b) => {
-            if ((this.props.user.type === 'admin' || this.props.user.type === 'contributor') || b.visible) {
-              return (
-                <div key={`${b._id}main`} className="post">
-
-                  <IndivBlog blog={b} key={b._id} user={this.props.user} visible={b.visible} changeKeyword={this.changeKeyword} />
-                  {(this.props.user.type === 'admin' || this.props.user.type === 'contributor')
-                    ? (
-                      <div key={`${b._id}div`}>
-                        <NavLink to={ROUTES.NEW_BLOG} onClick={() => { this.props.setBlog(b); }}>
-                          <div className="button" role="button" tabIndex={0}>
-                            Edit Post
-                          </div>
-                        </NavLink>
-                        <div onClick={() => {
-                          this.props.updateBlog(
-                            { ...b, visible: !b.visible },
-                            this.props.user,
-                          );
-                        }}
-                          role="button"
-                          tabIndex={0}
-                          className="button"
-                        >
-                          {b.visible ? 'Hide post' : 'Show post'}
-                        </div>
-                      </div>
-                    )
-                    : <div key={`${b._id}div`} />
-                    }
+          {this.state.keyword === '' ? <div />
+            : (
+              <div>
+                <p style={{ display: 'inline-block' }}>Keyword selected: {this.state.keyword}</p>
+                <div className="button" role="button" tabIndex={0} onClick={() => this.changeKeyword('')}>
+                  clear
                 </div>
-              );
+              </div>
+            )
+
+          }
+
+          {this.props.blogs.map((b) => {
+            if (this.state.keyword === '' || b.keywords.includes(this.state.keyword)) {
+              if ((this.props.user.type === 'admin' || this.props.user.type === 'contributor') || b.visible) {
+                return (
+                  <div key={`${b._id}main`} className="post">
+
+                    <IndivBlog blog={b} key={b._id} user={this.props.user} visible={b.visible} changeKeyword={this.changeKeyword} />
+                    {(this.props.user.type === 'admin' || this.props.user.type === 'contributor')
+                      ? (
+                        <div key={`${b._id}div`}>
+                          <NavLink to={ROUTES.NEW_BLOG} onClick={() => { this.props.setBlog(b); }}>
+                            <div className="button" role="button" tabIndex={0}>
+                              Edit Post
+                            </div>
+                          </NavLink>
+                          <div onClick={() => {
+                            this.props.updateBlog(
+                              { ...b, visible: !b.visible },
+                              this.props.user,
+                            );
+                          }}
+                            role="button"
+                            tabIndex={0}
+                            className="button"
+                          >
+                            {b.visible ? 'Hide post' : 'Show post'}
+                          </div>
+                        </div>
+                      )
+                      : <div key={`${b._id}div`} />
+                      }
+                  </div>
+                );
+              } else return (<div key={`${b._id}div`} />);
             } else return (<div key={`${b._id}div`} />);
+          })
+          }
+        </div>
+        <div style={{ display: 'inline-block' }}>
+          {this.props.keywords.map((kw) => {
+            return (
+              <div onClick={() => { this.changeKeyword(kw.name); }}
+                role="button"
+                tabIndex={0}
+                key={kw.name}
+                style={{
+                  display: 'inline-block', margin: '5px', textDecoration: 'underline', color: 'blue',
+                }}
+              >
+                {kw.name}
+              </div>
+            );
           })}
         </div>
       </div>
